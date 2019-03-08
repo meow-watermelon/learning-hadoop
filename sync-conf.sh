a6e8f6bd419a4e1002604cd4cb1f2b9e04910809
Watermelon <herdingcat@yahoo.com>
2019-03-07 22:34:23 -0800

    20190307 - add sync script + enable hdfs-audit log
    Signed-off-by: Watermelon <herdingcat@yahoo.com>

--- sync-conf.sh
+++ sync-conf.sh
@@ -7,12 +7,16 @@ cat hosts | sed '/nfs/d' | xargs -I {} scp conf/{core-site.xml,dfs.exclude,dfs.i
 
 echo "Syncing nn1 config..."
 scp conf/hdfs-site.xml.nn1 root@nn1.internal:/etc/hadoop/hdfs-site.xml
+scp conf/hadoop-env.sh.nn root@nn1.internal:/etc/hadoop/hadoop-env.sh
+scp conf/log4j.properties.nn root@nn1.internal:/etc/hadoop/log4j.properties
 
 echo "Syncing nn2 config..."
 scp conf/hdfs-site.xml.nn2 root@nn2.internal:/etc/hadoop/hdfs-site.xml
+scp conf/hadoop-env.sh.nn root@nn2.internal:/etc/hadoop/hadoop-env.sh
+scp conf/log4j.properties.nn root@nn2.internal:/etc/hadoop/log4j.properties
 
 echo "Syncing datanode config..."
-scp hdfs-site.xml.client root@dn1.internal:/etc/hadoop/hdfs-site.xml
-scp hdfs-site.xml.client root@dn2.internal:/etc/hadoop/hdfs-site.xml
-scp hdfs-site.xml.client root@dn3.internal:/etc/hadoop/hdfs-site.xml
-scp hdfs-site.xml.client root@dn4.internal:/etc/hadoop/hdfs-site.xml
+scp conf/hdfs-site.xml.client root@dn1.internal:/etc/hadoop/hdfs-site.xml
+scp conf/hdfs-site.xml.client root@dn2.internal:/etc/hadoop/hdfs-site.xml
+scp conf/hdfs-site.xml.client root@dn3.internal:/etc/hadoop/hdfs-site.xml
+scp conf/hdfs-site.xml.client root@dn4.internal:/etc/hadoop/hdfs-site.xml
